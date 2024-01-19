239cf810a9531f54ae7e1a165a686c2c3223788d
NihatKahraman <Nihat.Kahraman@d-teknoloji.com.tr>
2024-01-19 15:04:27 +0300

    Add project files.

---
+++ Entities/Exceptions/PriceOutofRangeBadRequestException.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities.Exceptions
+{
+    public abstract partial class BadRequestException
+    {
+        public class PriceOutofRangeBadRequestException : BadRequestException
+        {
+            public PriceOutofRangeBadRequestException() 
+                : base("Maxiumum price should ve less than 1000 and greater than 10.")
+            {
+
+            }
+        }
+    }
+}
